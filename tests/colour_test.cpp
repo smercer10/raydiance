3425290d721d6b00c34085296f0fb40db0cb150b
Sam Mercer <smercer10@proton.me>
2024-02-24 14:38:01 +0000

    Use EXPECT_DOUBLE_EQ for colour tests #9

--- tests/colour_test.cpp
+++ tests/colour_test.cpp
@@ -3,17 +3,16 @@
 
 TEST(ColourTest, ElementAccess) {
     colour c(0.5, 0.6, 0.7);
-    EXPECT_EQ(c.r(), 0.5);
-    EXPECT_EQ(c.g(), 0.6);
-    EXPECT_EQ(c.b(), 0.7);
+    EXPECT_DOUBLE_EQ(c.r(), 0.5);
+    EXPECT_DOUBLE_EQ(c.g(), 0.6);
+    EXPECT_DOUBLE_EQ(c.b(), 0.7);
 }
 
 TEST(ColourTest, Addition) {
     colour c1(0.001, 0.6, 0.7);
     colour c2(0.0, 0.1, 1.0);
     colour c3 = c1 + c2;
-
-    EXPECT_EQ(c3.r(), 0.001);
-    EXPECT_EQ(c3.g(), 0.7);
-    EXPECT_EQ(c3.b(), 1.7);
+    EXPECT_DOUBLE_EQ(c3.r(), 0.001);
+    EXPECT_DOUBLE_EQ(c3.g(), 0.7);
+    EXPECT_DOUBLE_EQ(c3.b(), 1.7);
 }
