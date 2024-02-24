d534dea528cea6d4f51b792c01c02446ec7b5d37
Sam Mercer <smercer10@proton.me>
2024-02-24 12:01:53 +0000

    Add vec3 helper functions #5

--- include/raydiance/vec3.h
+++ include/raydiance/vec3.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cmath>
+#include <ostream>
 
 namespace vec3 {
     class vec3 {
@@ -8,18 +10,93 @@ namespace vec3 {
         vec3() : e{0, 0, 0} {}
         vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        [[nodiscard]] double x() const;
-        [[nodiscard]] double y() const;
-        [[nodiscard]] double z() const;
+        [[nodiscard]] double x() const { return e[0]; }
+        [[nodiscard]] double y() const { return e[1]; }
+        [[nodiscard]] double z() const { return e[2]; }
 
-        vec3 operator-() const;         // Unary minus
-        double operator[](int i) const; // Read-only access
-        double &operator[](int i);      // Read-write access
-        vec3 &operator+=(const vec3 &v);// Vector addition
-        vec3 &operator*=(double t);     // Scalar multiplication
-        vec3 &operator/=(double t);     // Scalar division
+        // Negation
+        vec3 operator-() const { return {-e[0], -e[1], -e[2]}; }
 
-        [[nodiscard]] double length() const;
-        [[nodiscard]] double lengthSquared() const;
+        // Read-only access
+        double operator[](int i) const { return e[i]; }
+
+        // Read-write access
+        double &operator[](int i) { return e[i]; }
+
+        vec3 &operator+=(const vec3 &v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+
+            return *this;
+        }
+
+        // Scalar multiplication
+        vec3 &operator*=(double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+
+            return *this;
+        }
+
+        // Scalar division
+        vec3 &operator/=(double t) { return *this *= 1 / t; }
+
+        [[nodiscard]] double length() const { return std::sqrt(lengthSquared()); }
+
+        [[nodiscard]] double lengthSquared() const {
+            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+        }
     };
+
+    // For geometric clarity
+    using point3 = vec3;
+
+    // Example usage: outStream << vecObj;
+    inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+        return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+    }
+
+    inline vec3 operator+(const vec3 &u, const vec3 &v) {
+        return {u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]};
+    }
+
+    inline vec3 operator-(const vec3 &u, const vec3 &v) {
+        return {u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]};
+    }
+
+    // Hadamard product
+    inline vec3 operator*(const vec3 &u, const vec3 &v) {
+        return {u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]};
+    }
+
+    // Scalar multiplication
+    inline vec3 operator*(double t, const vec3 &v) {
+        return {t * v.e[0], t * v.e[1], t * v.e[2]};
+    }
+
+    // Also scalar multiplication
+    inline vec3 operator*(const vec3 &v, double t) {
+        return t * v;
+    }
+
+    // Scalar division
+    inline vec3 operator/(vec3 v, double t) {
+        return (1 / t) * v;
+    }
+
+    inline double dot(const vec3 &u, const vec3 &v) {
+        return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+    }
+
+    inline vec3 cross(const vec3 &u, const vec3 &v) {
+        return {u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]};
+    }
+
+    inline vec3 unitVector(vec3 v) {
+        return v / v.length();
+    }
 }// namespace vec3
