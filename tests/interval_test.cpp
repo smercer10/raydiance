55d94bf2df6651c8af09e62c2c3e515aea9cb803
Sam Mercer <smercer10@proton.me>
2024-02-25 13:17:59 +0000

    Test interval class methods #12

---
+++ tests/interval_test.cpp
@@ -0,0 +1,37 @@
+#include "raydiance/globals.h"
+#include "raydiance/interval.h"
+#include <gtest/gtest.h>
+
+TEST(IntervalTest, Contains) {
+    interval i(0, 1);
+    EXPECT_TRUE(i.contains(0));
+    EXPECT_TRUE(i.contains(1));
+    EXPECT_TRUE(i.contains(0.5));
+    EXPECT_FALSE(i.contains(-0.1));
+    EXPECT_FALSE(i.contains(2));
+}
+
+TEST(IntervalTest, Surrounds) {
+    interval i(0, 1);
+    EXPECT_TRUE(i.surrounds(0.1));
+    EXPECT_TRUE(i.surrounds(0.95));
+    EXPECT_FALSE(i.surrounds(0));
+    EXPECT_FALSE(i.surrounds(1));
+}
+
+TEST(IntervalTest, Empty) {
+    interval i = interval::empty;
+    EXPECT_FALSE(i.contains(0));
+    EXPECT_FALSE(i.contains(-50));
+    EXPECT_FALSE(i.surrounds(100));
+}
+
+TEST(IntervalTest, Universe) {
+    interval i = interval::universe;
+    EXPECT_TRUE(i.contains(-20));
+    EXPECT_TRUE(i.contains(5));
+    EXPECT_TRUE(i.contains(infinity));
+    EXPECT_FALSE(i.surrounds(infinity));
+    EXPECT_TRUE(i.contains(-infinity));
+    EXPECT_FALSE(i.surrounds(-infinity));
+}
