d20e38e7b6dfe11bd81da8cba2ebf01da8009eb6
Sam Mercer <smercer10@proton.me>
2024-02-25 13:16:43 +0000

    Test intersection class setFaceNormal method #12

---
+++ tests/object_test.cpp
@@ -0,0 +1,24 @@
+#include "raydiance/object.h"
+#include <gtest/gtest.h>
+
+TEST(IntersectionTest, SetFaceNormal) {
+    ray r1(point3{0, 0, 0}, vec3{0, 0, 1});
+    vec3 on1{0, 0, -1};
+    intersection i1;
+    i1.setFaceNormal(r1, on1);
+
+    EXPECT_TRUE(i1.frontFace);
+    EXPECT_DOUBLE_EQ(i1.normal.x(), 0);
+    EXPECT_DOUBLE_EQ(i1.normal.y(), 0);
+    EXPECT_DOUBLE_EQ(i1.normal.z(), -1);
+
+    ray r2(point3{0, 0, 0}, vec3{0, 0, -1});
+    vec3 on2{0, 0, -1};
+    intersection i2;
+    i2.setFaceNormal(r2, on2);
+
+    EXPECT_FALSE(i2.frontFace);
+    EXPECT_DOUBLE_EQ(i2.normal.x(), 0);
+    EXPECT_DOUBLE_EQ(i2.normal.y(), 0);
+    EXPECT_DOUBLE_EQ(i2.normal.z(), 1);
+}
