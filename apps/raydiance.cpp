199537f307cc7e55a491b00c6c422bc791211dae
Sam Mercer <smercer10@proton.me>
2024-02-28 19:26:29 +0000

    Get file paths from command line arguments #18
    A file path must be provided for the scene config file, whereas only an
    image name needs to be provided for the output image file.

--- apps/raydiance.cpp
+++ apps/raydiance.cpp
@@ -4,16 +4,48 @@
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <unistd.h>
 
-int main() {
-    std::ofstream imgOut{file::openOutStream("img")};
+int main(int argc, char *argv[]) {
+    std::string sceneConfigPath;
+    std::string imgOutName;
+
+    // Get file paths from command line arguments
+    int opt;
+    while ((opt = getopt(argc, argv, "s:o:")) != -1) {
+        switch (opt) {
+            case 's':
+                sceneConfigPath = optarg;
+                break;
+            case 'o':
+                imgOutName = optarg;
+                break;
+            default:
+                std::cerr << "Invalid option.\n\n"
+                          << "Usage: raydiance -s <scene config file path> [-o <output image name>]\n";
+                return 1;
+        }
+    }
+
+    if (sceneConfigPath.empty()) {
+        std::cerr << "Scene config file is required.\n\n"
+                  << "Usage: vb2c -s <scene config file path> [-o <output image name>]\n";
+        return 1;
+    }
+
+    // The default output image name is "img"
+    if (imgOutName.empty()) {
+        imgOutName = "img";
+    }
+
+    std::ofstream imgOut{file::openOutStream(imgOutName)};
+
+    std::ifstream sceneConfigFile{sceneConfigPath};
 
     if (!imgOut.is_open()) {
         std::cerr << "Failed to open file for writing.\n";
         return 1;
     }
-
-    std::ifstream sceneConfigFile{"scene.json"};
 
     if (!sceneConfigFile.is_open()) {
         std::cerr << "Failed to open scene config file.\n";
